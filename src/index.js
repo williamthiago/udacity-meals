11b335d5f8c1240ce68b26a10dc59e29128003e8
William Thiago <williamthiago@gmail.com>
2017-10-04 17:28:19 -0300

    feat: Add actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+       <App />
+    </Provider>
+, document.getElementById('root'))
